20ead116c197237b6360f430e2f4a2a86c86d039
Ashley Michelle <ashleysanchez@MacBook-Pro.local>
2025-10-21 10:52:33 -0400

    cluster-database

--- config/database.js
+++ config/database.js
@@ -1,17 +1,34 @@
-require('dotenv').config();
+// require('dotenv').config();
+// const mongoose = require('mongoose');
+
+// // Función para conectar a la base de datos
+// const connectDB = async () => {
+//   try {
+//     if (!process.env.MONGODB_URI) {
+//       throw new Error('MONGO_URI is not defined in the environment variables');
+//     }
+//     await mongoose.connect(process.env.MONGODB_URI);
+//     console.log('MongoDB connected successfully');
+//   } catch (error) {
+//     process.exit(1); // Salir del proceso con error
+//   }
+// };
+
+// module.exports = connectDB;
+
+require('dotenv').config(); 
 const mongoose = require('mongoose');
 
-// Función para conectar a la base de datos
 const connectDB = async () => {
   try {
     if (!process.env.MONGODB_URI) {
-      throw new Error('MONGO_URI is not defined in the environment variables');
+      throw new Error('MONGODB_URI is not defined in the environment variables');
     }
     await mongoose.connect(process.env.MONGODB_URI);
     console.log('MongoDB connected successfully');
   } catch (error) {
-    process.exit(1); // Salir del proceso con error
+    console.error('MongoDB connection error:', error.message); // ADD THIS LINE
+    process.exit(1);
   }
 };
-
 module.exports = connectDB;
